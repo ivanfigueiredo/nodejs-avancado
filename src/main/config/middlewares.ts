51c74b05722d8b70ba19f2fed14643cb4e1ef813
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-12-21 08:26:47 -0300

    feat: add middleware urlencoded

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
-import { Express } from 'express'
 import { BodyParser, cors, contentType } from '../middlewares/index'
+import { Express, urlencoded } from 'express'
 
 export default (app: Express): void => {
   app.use(BodyParser)
   app.use(cors)
+  app.use(urlencoded({ extended: true }))
   app.use(contentType)
 }
