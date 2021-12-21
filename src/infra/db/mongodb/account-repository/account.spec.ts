e2e9b827fe46f24f23f742f74a5ec8a7975545f7
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-12-21 17:51:24 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,18 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const res = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(res.ops[0].accessToken).toBeFalsy()
+    await sut.updateAccessToken(res.ops[0]._id, 'any_token')
+    const account = await accountCollection.findOne({ _id: res.ops[0]._id })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
+  })
 })
