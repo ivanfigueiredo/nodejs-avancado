8951c61e69f07c48a2546e92dd09fc71c1b043de
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-12-22 11:46:59 -0300

    refactor: move LogErrorRepository to a folder

--- src/main/decorators/log-controller-decorator.spec.ts
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -1,7 +1,7 @@
 import { ServerError } from '../../presentation/errors'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log-controller-decorator'
-import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log/log-error-repository'
 import { serverError, ok } from '../../presentation/helpers/http/http-helper'
 import { AccountModel } from '../../domain/models/account'
 
