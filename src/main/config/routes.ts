4b4a6a9b0fb5bdea057b0a77e653664707d1c6f4
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-12-22 17:04:10 -0300

    chore: add files

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,11 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-import { join } from 'path'
+import { resolve, join } from 'path'
 
 export default (app: Express): void => {
-  const fullPath = join(__dirname, '/../routes')
   const router = Router()
   app.use('/api', router)
-  readdirSync(fullPath).map(async file => {
+  readdirSync(resolve(__dirname, '../', 'routes')).map(async file => {
     if (!file.includes('.test.')) {
       (await import(`../routes/${file}`)).default(router)
     }
--- src/main/routes/signup-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { makeSignUpController } from '../factories/signup'
-import { adaptRoute } from '../adapters/express-route-adapter'
-
-export default (router: Router): void => {
-  router.post('/signup', adaptRoute(makeSignUpController()))
-}
