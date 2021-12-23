5a5cd61dbd7ccd927d52f1348ed410b18848b166
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-12-23 15:12:42 -0300

    feat: ensure SignUpController returns 500 if Authentication throws

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -121,4 +121,11 @@ describe('Signup Controller', () => {
       password: 'any_password'
     })
   })
+
+  test('Should return 500 if EmailValidator throws', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
