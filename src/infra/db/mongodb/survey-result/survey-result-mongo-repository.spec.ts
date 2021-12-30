f4095c11f474c3a810d8187528512c377de30fca
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-12-30 10:45:19 -0300

    fix: change save survey result to return all answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -19,7 +19,7 @@ const makeSurvey = async (): Promise<SurveyModel> => {
       image: 'any_image',
       answer: 'any_answer'
     }, {
-      answer: 'any_answer'
+      answer: 'other_answer'
     }],
     date: new Date()
   })
@@ -57,13 +57,20 @@ describe('Survey Mongo Repository', () => {
     test('Should add a survey result if its new', async () => {
       const survey = await makeSurvey()
       const account = await makeAccount()
-      const surveyResult = await surveyResultCollection.insertOne({
+      const sut = makeSut()
+      const surveyResult = await sut.save({
         surveyId: survey.id,
         accountId: account.id,
         answer: survey.answers[0].answer,
         date: new Date()
       })
       expect(surveyResult).toBeTruthy()
+      expect(surveyResult.surveyId).toEqual(survey.id)
+      expect(surveyResult.answers[0].answer).toBe(survey.answers[0].answer)
+      expect(surveyResult.answers[0].count).toBe(1)
+      expect(surveyResult.answers[0].percent).toBe(100)
+      expect(surveyResult.answers[1].count).toBe(0)
+      expect(surveyResult.answers[1].percent).toBe(0)
     })
   })
 
@@ -71,28 +78,20 @@ describe('Survey Mongo Repository', () => {
     test('Should update survey result if its not new', async () => {
       const survey = await makeSurvey()
       const account = await makeAccount()
-      await surveyResultCollection.insertOne({
-        surveyId: new ObjectId(survey.id),
-        accountId: new ObjectId(account.id),
-        answer: survey.answers[0].answer,
-        date: new Date()
-      })
       const sut = makeSut()
-      await sut.save({
+      const surveyResult = await sut.save({
         surveyId: survey.id,
         accountId: account.id,
         answer: survey.answers[1].answer,
         date: new Date()
       })
-      const surveyResult = await surveyResultCollection
-        .find({
-          surveyId: new ObjectId(survey.id),
-          accountId: new ObjectId(account.id)
-        })
-        .toArray()
       expect(surveyResult).toBeTruthy()
-      expect(surveyResult.length).toBe(1)
-      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.surveyId).toEqual(survey.id)
+      expect(surveyResult.answers[0].answer).toBe(survey.answers[1].answer)
+      expect(surveyResult.answers[0].count).toBe(1)
+      expect(surveyResult.answers[0].percent).toBe(100)
+      expect(surveyResult.answers[1].count).toBe(0)
+      expect(surveyResult.answers[1].percent).toBe(0)
     })
   })
 })
