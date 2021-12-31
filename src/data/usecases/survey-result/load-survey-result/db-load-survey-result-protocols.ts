fccfe718bc20cb39f742046f96a07b1dac03d3fb
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-12-31 12:25:16 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyByIdRepo if LoadSurveyResultRepo returns null

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../../domain/usecases/survey-result/load-survey-result'
 export * from '../../../../domain/models/survey-result'
 export * from '../../../protocols/db/survey-result/load-survey-result-repository'
+export * from './../../../protocols/db/survey/load-survey-by-id-repository'
--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -1,10 +1,16 @@
-import { LoadSurveyResultRepository, LoadSurveyResult, SurveyResultModel } from './db-load-survey-result-protocols'
+import { LoadSurveyResultRepository, LoadSurveyResult, SurveyResultModel, LoadSurveyByIdRepository } from './db-load-survey-result-protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
-  constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+  constructor (
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
+    private readonly loadSurveyByIdtRepository: LoadSurveyByIdRepository
+  ) {}
 
   async load (surveyId: string): Promise<SurveyResultModel> {
     const surveyResult = await this.loadSurveyResultRepository.loadBySurveyById(surveyId)
+    if (!surveyResult) {
+      await this.loadSurveyByIdtRepository.loadById(surveyId)
+    }
     return surveyResult
   }
 }
