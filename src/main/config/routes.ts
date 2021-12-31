b1f56f62a9675dfb3a732eceb8d9d66edcf4871c
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-12-31 17:43:12 -0300

    refactor: remove import join unitized

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,6 +1,6 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-import { resolve, join } from 'path'
+import { resolve } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
